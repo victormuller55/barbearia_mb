f4135967addd9e4387e4f990be4c2e746d3929ed
Vitor Bertoldo <vitorbertoldo10@gmail.com>
2024-04-29 16:56:56 -0300

    m

---
+++ options/services/adicionar_services.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendamento</title>
+    <style>
+        body {
+            font-family: 'Courier New', Courier, monospace;
+        }
+        
+        h1 {
+            text-align: center;
+        }
+
+        form {
+            text-align: center;
+        }
+
+        input[type="text"] {
+
+        }
+    </style>
+</head>
+<body>
+    <h1>Agendamento</h1>    
+
+    <form  method="post" action="">
+        <input type="text" name="nome_servico" placeholder="Digite o nome do cliente"><br>
+        <input type="text" name="funcionario_servico" placeholder="Digite o nome do funcionário"><br>
+        <input type="text" name="servico" placeholder="Digite o serviço"><br>
+        <input type="text" name="servico" placeholder="Digite a data/hora"><br>
+        <input type="text" name="servico" placeholder="Observaçôes"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
